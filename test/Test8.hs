2337f2558d303915f61f2c4ed0a1a942a25549f5
tammosminia <tammosminia@gmail.com>
2019-12-14 12:47:42 +0100

    advent

--- test/Test8.hs
+++ test/Test8.hs
@@ -10,3 +10,7 @@ main = hspec $ do
     it "layerCounts" $ do
       countLayer "120120120" `shouldBe` [3, 3, 3]
       countLayer "120121" `shouldBe` [1, 3, 2]
+      
+    it "render" $ do
+      render (splitEvery 4 "0222112222120000") `shouldBe` "0110"
+  
