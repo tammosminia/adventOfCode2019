6cc65bdea085e6b3b592f2f225f253918a6e528f
tammosminia <tammosminia@gmail.com>
2019-12-16 17:02:01 +0100

    advent

--- src/Day10.hs
+++ src/Day10.hs
@@ -3,6 +3,7 @@ module Day10 where
 import Data.List
 
 input1 = ["#.#.###.#.#....#..##.#....",".....#..#..#..#.#..#.....#",".##.##.##.##.##..#...#...#","#.#...#.#####...###.#.#.#.",".#####.###.#.#.####.#####.","#.#.#.##.#.##...####.#.##.","##....###..#.#..#..#..###.","..##....#.#...##.#.#...###","#.....#.#######..##.##.#..","#.###.#..###.#.#..##.....#","##.#.#.##.#......#####..##","#..##.#.##..###.##.###..##","#..#.###...#.#...#..#.##.#",".#..#.#....###.#.#..##.#.#","#.##.#####..###...#.###.##","#...##..#..##.##.#.##..###","#.#.###.###.....####.##..#","######....#.##....###.#..#","..##.#.####.....###..##.#.","#..#..#...#.####..######..","#####.##...#.#....#....#.#",".#####.##.#.#####..##.#...","#..##..##.#.##.##.####..##",".##..####..#..####.#######","#.#..#.##.#.######....##..",".#.##.##.####......#.##.##"]
+example1 = [".#..#",".....","#####","....#","...##"]
 
 type Position = (Int, Int) -- (x,y)  (0,0) is top left
 
@@ -19,15 +20,34 @@ subtractPos :: Position -> Position -> Position
 subtractPos (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
 
 blocks :: Position -> Position -> Position -> Bool
-blocks from to middle = blocks0 (subtractPos middle from) (subtractPos to from)
-  where blocks0 (x1, y1) (x2, y2) = (mod x2 x1) == 0 && (mod y2 y1) == 0 && (div x2 x1) == (div y2 y1)
+blocks from middle to = blocks0 (subtractPos middle from) (subtractPos to from)
+  where 
+    blocks0 (x1, y1) (x2, y2) = (isExactMultiple x1 x2) && (isExactMultiple y1 y2) && isSameMultiple (x1, y1) (x2, y2)
+    isExactMultiple x1 x2
+      | x1 == x2 = True
+      | x1 == 0 = False
+      | x2 == 0 = False
+      | otherwise = (mod x2 x1) == 0
+    isSameMultiple (x1, y1) (x2, y2)
+      | x1 == 0 && x2 == 0 = True
+      | y1 == 0 && y2 == 0 = True
+      | x1 == 0 || y1 == 0 = False
+      | otherwise = (div x2 x1) == (div y2 y1)
 
 visibles :: Position -> [Position] -> [Position]
 visibles from all = filter (visible all from) all
 
 visible :: [Position] -> Position -> Position -> Bool
-visible all from to = any (blocks from to) (all \\ [from, to])
+visible all from to = any (\x -> blocks from x to) (all \\ [from, to])
 
+bestAsteroid :: [String] -> (Int, Position)
+bestAsteroid i = last $ sort $ map (\(s, vs) -> (length vs, s)) $ allVisible i
+
+allVisible :: [String] -> [(Position, [Position])]
+allVisible i = map (\x -> (x, visibles x positions)) positions
+  where positions = parseInput i
+    
+  
 positions1 = parseInput input1
 visibles1 = map (\x -> (x, visibles x positions1)) positions1
-answer1 = head $ sort $ map (\(s, vs) -> (length vs, s)) visibles1
+answer1 = bestAsteroid input1
--- test/Test10.hs
+++ test/Test10.hs
@@ -3,6 +3,7 @@ import Test.QuickCheck
 import Control.Exception (evaluate)
 import Day10
 
+
 main :: IO ()
 main = hspec $ do
 
@@ -11,8 +12,19 @@ main = hspec $ do
       parseInput ["#.#",".#.","#.#"] `shouldBe` [(0,0), (2,0), (1,1), (0,2), (2,2)]
 
     it "blocks" $ do
-      blocks (0,0) (2,2) (1,1) `shouldBe` True
-      blocks (0,0) (2,3) (1,1) `shouldBe` False
-      blocks (0,0) (1,1) (2,2) `shouldBe` False
-      blocks (0,0) (10,10) (2,2) `shouldBe` True
-      blocks (0,0) (8,10) (2,2) `shouldBe` False
+      blocks (0,0) (1,1) (2,2) `shouldBe` True
+      blocks (0,0) (1,1) (2,3) `shouldBe` False
+      blocks (0,0) (2,2) (1,1) `shouldBe` False
+      blocks (0,0) (2,2) (10,10) `shouldBe` True
+      blocks (0,0) (2,2) (8,10) `shouldBe` False
+      blocks (0,0) (0,0) (2,2) `shouldBe` False
+      blocks (0,0) (2,2) (0,0) `shouldBe` False
+      blocks (0,0) (0,1) (0,2) `shouldBe` True
+      
+    it "example1" $ do
+      bestAsteroid example1 `shouldBe` (8,(3,4))
+  
+--    it "example2" $ do
+--      bestAsteroid ["......#.#.","#..#.#....","..#######.",".#.#.###..",".#..#.....","..#....#.#","#..#....#.",".##.#..###","##...#..#.",".#....####"] `shouldBe` (33,(5,8))
+  
+      
